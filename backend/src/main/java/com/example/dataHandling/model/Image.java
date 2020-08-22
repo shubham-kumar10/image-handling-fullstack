411ca45a4cec5528449536e959ca17e627966ed8
Shubham Kumar <shubham.snu@gmail.com>
2020-08-22 23:59:10 +0530

    Added constructor

--- backend/src/main/java/com/example/dataHandling/model/Image.java
+++ backend/src/main/java/com/example/dataHandling/model/Image.java
@@ -41,37 +41,4 @@ public class Image {
 	
 	@Column(name = "picByte", length = 1000)
 	private byte picByte[];
-
-//	public long getId() {
-//		return id;
-//	}
-//
-//	public void setId(long id) {
-//		this.id = id;
-//	}
-//
-//	public String getName() {
-//		return name;
-//	}
-//
-//	public void setName(String name) {
-//		this.name = name;
-//	}
-//
-//	public String getType() {
-//		return type;
-//	}
-//
-//	public void setType(String type) {
-//		this.type = type;
-//	}
-//
-//	public byte[] getPicByte() {
-//		return picByte;
-//	}
-//
-//	public void setPicByte(byte[] picByte) {
-//		this.picByte = picByte;
-//	}
-	
 }
